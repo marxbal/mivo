3c17fe0af339d824fbfc3e9c0ca132011d0fecc6
Jethru Balarbar <marx.balarbar@tooltwist.com>
2020-11-06 16:36:09 +0800

    Update quotation-home.component.ts

--- src/app/components/quotation-home/quotation-home.component.ts
+++ src/app/components/quotation-home/quotation-home.component.ts
@@ -191,7 +191,7 @@ export class QuotationHomeComponent implements OnInit, AfterViewChecked {
       right: ['', Validators.required],
       left: ['', Validators.required],
       rear: ['', Validators.required],
-      improvement: [null, validateNumber],
+      improvement: ['', [validateNumber]],
       relatedStructure: this.fb.array([]),
       relatedContent: this.fb.array([]),
 
