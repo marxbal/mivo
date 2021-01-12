e71653d889af75e37b685c714be34eae613ac82b
Jethru Balarbar <marx.balarbar@tooltwist.com>
2021-01-13 06:45:38 +0800

    subAgent to subAgents

--- src/app/components/quotation-car/quotation-car.component.ts
+++ src/app/components/quotation-car/quotation-car.component.ts
@@ -344,7 +344,7 @@ export class QuotationCarComponent implements OnInit, AfterViewChecked {
       paymentMethod: ['', Validators.required],
       product: ['', Validators.required],
       //subagent
-      subAgent: [null],
+      subAgents: [null],
     });
   }
 
