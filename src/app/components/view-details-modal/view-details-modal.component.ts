c5d651842f94a989a239a48eee6f1fd46b17f1a2
Jethru Balarbar <marx.balarbar@tooltwist.com>
2021-01-22 20:25:28 +0800

    css

--- src/app/components/view-details-modal/view-details-modal.component.ts
+++ src/app/components/view-details-modal/view-details-modal.component.ts
@@ -1,7 +1,18 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ListClientDetails } from 'src/app/objects/ListClientDetails';
+import {
+  Component,
+  Inject,
+  OnInit
+} from '@angular/core';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material';
+import {
+  BsModalRef
+} from 'ngx-bootstrap/modal';
+import {
+  ListClientDetails
+} from 'src/app/objects/ListClientDetails';
 
 @Component({
   selector: 'app-view-details-modal',
@@ -16,11 +27,14 @@ export class ViewDetailsModalComponent implements OnInit {
   modalRef: BsModalRef;
 
   constructor(public dialogRef: MatDialogRef < ViewDetailsModalComponent > ,
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    private bms: BsModalService) { }
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit(): void {
     this.listClientDetails = this.data;
+  }
+
+  close(): void {
+    this.dialogRef.close();
   }
 
 }
