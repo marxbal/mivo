7a7c29e9d149ae874b67a1af6ab022edc5e26d9f
Ken Layug <layugkm@mapfreinsurance.com.ph>
2021-03-04 09:47:34 +0800

    updated payment urls.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  baseUrl: 'https://prd2.mapfreinsurance.com.ph/mivo2',
   imgUrl: '/mivo',
   apiUrl: 'https://prd2.mapfreinsurance.com.ph/tronws/',
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  baseUrl: 'https://prd2.mapfreinsurance.com.ph/mivo2',
   imgUrl: '/mivo2',
   apiUrl: 'https://prd2.mapfreinsurance.com.ph/tronws/',
 };
