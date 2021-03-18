5ad36bdca6911b0997d4a7118fded069e17be8c5
Ken Layug <layugkm@mapfreinsurance.com.ph>
2021-03-18 09:28:16 +0800

    updated prod env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://mivo.mapfreinsurance.com.ph/mivo2',
+  baseUrl: 'https://mivo.mapfreinsurance.com.ph/mivo',
   imgUrl: '/mivo',
   apiUrl: 'https://mivo.mapfreinsurance.com.ph/tronws/',
 };
